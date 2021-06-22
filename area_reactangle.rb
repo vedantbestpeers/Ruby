ad333b1f09ffa41054a45fd1f69de139c0730e39
vedantbestpeers <vebansal@bestpeers.com>
2021-06-22 19:51:25 +0530

    add program for area of square & reactangle

---
+++ area_reactangle.rb
@@ -0,0 +1,6 @@
+puts 'Enter the length Number'
+a = gets.chomp.to_f
+puts 'Enter the Breadth'
+b = gets.chomp.to_f
+area = a * b
+puts "the area of Rectangle is #{area}"
---
+++ area_square.rb
@@ -0,0 +1,3 @@
+puts 'Enter the length of square'
+x = gets.chomp.to_i
+puts 4 * x
